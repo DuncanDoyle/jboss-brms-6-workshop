25d229be522c0f67d34fdb660b7939db943ad83c
Duncan Doyle <Duncan.Doyle@gmail.com>
2016-03-08 18:32:17 +0200

    Fixed some date errors. Started reimplementing Lab 8 by removing unrequired classes.

--- Brms6WorkshopTicketsUseCaseRules4/src/main/java/org/jboss/ddoyle/brms/workshop/Main.java
+++ Brms6WorkshopTicketsUseCaseRules4/src/main/java/org/jboss/ddoyle/brms/workshop/Main.java
@@ -8,7 +8,7 @@ public class Main extends RulesEngine {
 
 	public static void main(String[] args) {
 		Main main = new Main();
-		main.run(new Person("Jason", "Doyle", new LocalDate(2011, 3, 11)), true);
+		main.run(new Person("Jason", "Doyle", new LocalDate(2010, 3, 11)), true);
 	}
 	
 	
